545d14555e75300290188d6d7bef6cdd8d912887
Dan Johnson <dsj7419@gmail.com>
2024-09-02 09:24:06 -0700

    Updated server.js to address TypeScript and ESLint warnings

--- server.js
+++ server.js
@@ -1,18 +1,23 @@
-const { createServer } = require('http');
-const { parse } = require('url');
-const next = require('next');
+import { createServer } from 'http';
+import { parse } from 'url';
+import next from 'next';
 
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT ?? '3000';
 
-app.prepare().then(() => {
+void app.prepare().then(() => {
   createServer((req, res) => {
-    const parsedUrl = parse(req.url, true);
-    handle(req, res, parsedUrl);
-  }).listen(port, (err) => {
+    if (req.url) {
+      const parsedUrl = parse(req.url, true);
+      void handle(req, res, parsedUrl);
+    } else {
+      res.writeHead(400);
+      res.end('Bad Request');
+    }
+  }).listen(port, (err?: Error) => {
     if (err) throw err;
     console.log(`> Ready on http://localhost:${port}`);
   });

fd685e7649688c3f79398b99743bbdf10a180b7a
Dan Johnson <dsj7419@gmail.com>
2024-09-02 12:01:00 -0700

    Fix TypeScript error in server file

--- server.ts
+++ server.ts
@@ -6,15 +6,15 @@ const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
-const port = process.env.PORT ?? '3000';
+const port = process.env.PORT || 3000;
 
-void app.prepare().then(() => {
+app.prepare().then(() => {
   createServer((req, res) => {
     if (req.url) {
       const parsedUrl = parse(req.url, true);
-      void handle(req, res, parsedUrl);
+      handle(req, res, parsedUrl);
     } else {
-      res.writeHead(400);
+      res.statusCode = 400;
       res.end('Bad Request');
     }
   }).listen(port, (err?: Error) => {

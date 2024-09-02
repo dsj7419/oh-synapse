19196296d7796188c68b114f69f4b4dc29f89375
Dan Johnson <dsj7419@gmail.com>
2024-09-02 08:43:40 -0700

    setting up railway service

--- next.config.js
+++ next.config.js
@@ -1,24 +1,30 @@
 /** @type {import("next").NextConfig} */
 const config = {
-    reactStrictMode: true,
-    output: 'standalone',
-    images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 'utfs.io',
-          port: '',
-          pathname: '/**',
-        },
-      ],
-    },
-    typescript: {
-      ignoreBuildErrors: true,
-    },
-    eslint: {
-      ignoreDuringBuilds: true,
-    },
-  };
-  
-  export default config;
-  
+  reactStrictMode: true,
+  output: 'standalone',
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'utfs.io',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  webpack: (config) => {
+    config.externals.push({
+      "utf-8-validate": "commonjs utf-8-validate",
+      bufferutil: "commonjs bufferutil"
+    });
+    return config;
+  },
+};
+
+export default config;
